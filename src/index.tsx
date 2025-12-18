0facdabe0b72fb9b2c7b0748c456b057a372af6d
Mihail Tsankov <jatvarq@gmail.com>
2025-12-18 11:27:09 +0200

    Removed the CreateReactApp registration (install prompt)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
+// src/index.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,7 +14,19 @@ root.render(
     </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+// Explicitly unregister any service workers to prevent the PWA install prompt
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+        for (const registration of registrations) {
+            registration.unregister().catch(() => void 0)
+        }
+    })
+    .catch(() => void 0)
+
+    // Also try to unregister the active registration (defensive)
+    navigator.serviceWorker.ready.then(registration => {
+        registration?.unregister().catch(() => void 0)
+    }).catch(() => void 0)
+}
